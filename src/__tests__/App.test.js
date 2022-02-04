4f804176fd4c2fade4487e5e4196ce7998b448e3
Mong'are <Mongaresams@gmail.com>
2022-02-04 15:51:18 +0300

    Fix linters error

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,7 +3,7 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import pretty from 'pretty';
 import { Provider } from 'react-redux';
-import store from '../Redux/configureStore';
+import store from '../Redux/configureStore.js';
 import App from '../App';
 
 let appContainer = null;
